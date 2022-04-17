e665ca09bbcf71b0ed610552accfe6d1c8aa7a6e
nevisende <ekonomitakipmail@gmail.com>
2022-04-17 20:43:52 +0300

    Feat: add Redis to project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const redis = require("redis");
 
 const {
   MONGO_USER,
@@ -10,30 +10,59 @@ const {
   MONGO_IP,
   MONGO_PORT,
   REDIS_URL,
-  SESSION_SECRET,
   REDIS_PORT,
 } = require("./config/config");
 
 const playerRoutes = require('./routes/players')
 
+const DB_CONNECTION_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectDbWithRetry = () => {
+  mongoose
+    .connect(DB_CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("successfully connected to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectDbWithRetry, 5000);
+    });
+};
+
+connectDbWithRetry();
+
+const redisClient = redis.createClient({ url: `redis://${REDIS_URL}:${REDIS_PORT}`});
+
+redisClient.on("connect", function () {
+  console.log("successfully connected to redis");
+});
+
+
+redisClient.on("error", function (err) {
+  console.error("fail connection of redis" + err);
+});
+
 const app = express()
+
 app.enable("trust proxy")
-dotenv.config()
 app.use(bodyParser.json({ limit: '50mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(cors())
 
-app.get("/", (req, res) => {
-  res.json({
-    author: "Furkan Denizhan",
-    message: "The index route of Game board server "
+app.get("/api/v1", (req, res) => {
+  res.json({ 
+    status: 'success',   
+    data: { 
+      author: "Furkan Denizhan",
+      message: "Welcome to my game board server :)"   
+    },
   })
 })
 
 app.use("/api/v1/players", playerRoutes)
 
 const PORT = process.env.PORT || 4400
-const DB_CONNECTION_URL = process.env.DB_CONNECTION_URL
 
 mongoose
   .connect(DB_CONNECTION_URL, {
@@ -41,10 +70,11 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Game board server is listening on ${PORT}`)
-    })
+    
   })
   .catch ((err) => {
     console.error(err)
   })
+
+app.listen(PORT, () => { console.log(`Game board server is listening on ${PORT}`) })
+exports.redisClient;
