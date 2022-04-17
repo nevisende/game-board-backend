5d3311638e34781b3454d29e8771431619ed5189
nevisende <ekonomitakipmail@gmail.com>
2022-04-17 16:00:17 +0300

    Feat: update player model and add isThisWeekActive

--- models/players.js
+++ models/players.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
 
 const playerSchema = mongoose.Schema({
-  username: String,
-  country: String,
-  money: Number,
+  username: {
+    type: String,
+    require: [true, "Players must have a username"],
+    unique: true,
+  },
+  country: {
+    type: String,
+    require: [true, "Players must have a country"],
+  },
+  money: {
+    type: Number,
+    default: 0,
+  },
+  isThisWeekActive: {
+    type: Boolean,
+    default: false,
+  },
 })
 
 const Player = mongoose.model('Player', playerSchema)
